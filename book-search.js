cad08136fe9582deba283d68b2cc7395776a96e6
Saifur00e <saifurbgc@gmaile.com>
2021-09-02 19:41:56 +0600

    fourth commit

--- book-search.js
+++ book-search.js
@@ -4,18 +4,17 @@ const bookDisplay = document.getElementById('book-display');
 const noSearchResult = document.getElementById('no-result');
 const totalBooksFound = document.getElementById('search-section');
 const div1 = document.createElement('div');
-var default_avatar = 'https://secure.gravatar.com/avatar?d=wavatar';
-
 
 
 searchBtn.addEventListener('click', ()=>{
+    bookDisplay.innerHTML = '';
     const bookNameEntered = searchByBookName.value;
     if(bookNameEntered === ''){
         noSearchResult.innerText = "Enter a book name first";
+        div1.innerHTML = "";
         return;
     }
     searchByBookName.value = '';
-    bookDisplay.innerHTML = '';
     
     // console.log(bookNameEntered);
     const url = `https://openlibrary.org/search.json?q=${bookNameEntered}`;
@@ -36,10 +35,11 @@ const displayData = data =>{
     // console.log(fetchedData);
     const totalSearchResults = fetchedData.length;
     if(totalSearchResults>0)
-    {div1.innerHTML=`<p id="totalBooks" class="text-center mt-4">Total Books Found : ${totalSearchResults}</p>`;
+    {div1.innerHTML=`<p id="totalBooks" class="text-center mt-4">Search Results : ${totalSearchResults}</p>`;
     totalBooksFound.appendChild(div1);}
     else{
         noSearchResult.innerText = "NO RESULTS FOUND"
+        div1.innerHTML = "";
     }
 
     fetchedData.forEach(item => {
@@ -48,25 +48,30 @@ const displayData = data =>{
         
         const div2 = document.createElement('div');
         div2.classList.add('col-md-4');
+        console.log(item.cover_i);
+        let imgUrl = item.cover_i? `https://covers.openlibrary.org/b/id/${item.cover_i}-L.jpg` : `https://orsa.tech/wp-content/uploads/2018/07/dummy-placeholder-image-400x400.jpg`;
+
         div2.innerHTML=`
-        <div class="rounded overflow-hidden border p-3 text-center ">
-            <img id="img-div" src="https://covers.openlibrary.org/b/id/${item.cover_i}-L.jpg" alt="" />
-        </div>
-        <div
-            class="
-            py-2
-            d-flex
-            justify-content-between
-            align-items-center
-            d-md-block
-            text-md-center
-            "
-        >
-            <h4>Book Name: ${item.title}</h4>
-            <p>Author Name: ${item.author_name!==undefined?item.author_name[0]: 'not available'}</p>
-            <p>First Publish Year : ${item.first_publish_year!== undefined? item.first_publish_year: 'not available'}</p>
-            <p id="publisher">Publisher : ${item.publisher!==undefined?item.publisher[0]: 'not available'}</p>
-            <button class="btn btn-dark">More Details</button>
+        <div class="card h-auto mb-4">
+            <div class="rounded overflow-hidden border p-3 text-center ">
+                <img class="img-fluid" src=${imgUrl} alt="" />
+            </div>
+            <div
+                class="
+                py-2
+                d-flex
+                justify-content-between
+                align-items-center
+                d-md-block
+                text-md-center
+                "
+            >
+                <h4>Book Name: ${item.title}</h4>
+                <p>Author Name: ${item.author_name!==undefined?item.author_name[0]: 'not available'}</p>
+                <p>First Publish Year : ${item.first_publish_year!== undefined? item.first_publish_year: 'not available'}</p>
+                <p id="publisher">Publisher : ${item.publisher!==undefined?item.publisher[0]: 'not available'}</p>
+                <button class="btn btn-dark">More Details</button>
+            </div>
         </div>
         `;
         // if(item.cover_i === undefined){
