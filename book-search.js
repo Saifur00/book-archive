e7c70a5c8ba39d4cd818a6173ae31619d08bd094
Saifur00e <saifurbgc@gmaile.com>
2021-09-02 18:54:40 +0600

    commit 3rd

--- book-search.js
+++ book-search.js
@@ -4,10 +4,15 @@ const bookDisplay = document.getElementById('book-display');
 const noSearchResult = document.getElementById('no-result');
 const totalBooksFound = document.getElementById('search-section');
 const div1 = document.createElement('div');
+var default_avatar = 'https://secure.gravatar.com/avatar?d=wavatar';
+
+
+
 searchBtn.addEventListener('click', ()=>{
     const bookNameEntered = searchByBookName.value;
     if(bookNameEntered === ''){
         noSearchResult.innerText = "Enter a book name first";
+        return;
     }
     searchByBookName.value = '';
     bookDisplay.innerHTML = '';
@@ -17,9 +22,9 @@ searchBtn.addEventListener('click', ()=>{
     fetch(url)
     .then(res => res.json())
     .then(data => {
-        // if(data.numFound !== 0){
-        //     noSearchResult.innerText ='';
-        // }
+        if(data){
+            noSearchResult.innerText ='';
+        }
         displayData(data);
     });
 }
@@ -30,16 +35,22 @@ const displayData = data =>{
     const fetchedData = data.docs;
     // console.log(fetchedData);
     const totalSearchResults = fetchedData.length;
-    div1.innerHTML=`<p id="totalBooks" class="text-center mt-4">Total Books Found : ${totalSearchResults}</p>`;
-    totalBooksFound.appendChild(div1);
+    if(totalSearchResults>0)
+    {div1.innerHTML=`<p id="totalBooks" class="text-center mt-4">Total Books Found : ${totalSearchResults}</p>`;
+    totalBooksFound.appendChild(div1);}
+    else{
+        noSearchResult.innerText = "NO RESULTS FOUND"
+    }
+
     fetchedData.forEach(item => {
         // console.log(item.cover_i);
         // console.log(item.title);
+        
         const div2 = document.createElement('div');
         div2.classList.add('col-md-4');
         div2.innerHTML=`
-        <div class="rounded overflow-hidden border p-3 text-center img-div">
-            <img src= "https://covers.openlibrary.org/b/id/${item.cover_i}-M.jpg"  alt="" />
+        <div class="rounded overflow-hidden border p-3 text-center ">
+            <img id="img-div" src="https://covers.openlibrary.org/b/id/${item.cover_i}-L.jpg" alt="" />
         </div>
         <div
             class="
@@ -52,35 +63,18 @@ const displayData = data =>{
             "
         >
             <h4>Book Name: ${item.title}</h4>
-            <p>Author Name: ${item.author_name}</p>
-            <p>First Publish Year : ${item.first_publish_year}</p>
-            <p>Publisher : ${item.publisher}</p>
+            <p>Author Name: ${item.author_name!==undefined?item.author_name[0]: 'not available'}</p>
+            <p>First Publish Year : ${item.first_publish_year!== undefined? item.first_publish_year: 'not available'}</p>
+            <p id="publisher">Publisher : ${item.publisher!==undefined?item.publisher[0]: 'not available'}</p>
             <button class="btn btn-dark">More Details</button>
         </div>
         `;
-        // const a = document.getElementsByTagName('img');
-        // if(typeof item.cover_i === 'undefined'){
-        //     // a.src="facebook_profile_image";
-        //     const imgDiv= document.getElementsByClassName('img-div');
-            
-        //     // imgDiv.innerHTML = `<img src= "facebook_profile_image"  alt="" />`;
-        //     // console.log(imgDiv.innerHTML);
-        //     // console.log(imgDiv.length);
-        //     // document.getElementById('book-display').appendChild(imgDiv);
-        // 
-        // console.log(item);
-        
-        const newArr = item.publisher.filter(items=> items!==undefined);
-        console.log(newArr);
-        // if(type of (${item.publisher})=== undefined ){
-        //     console.log("nothing to display");
+        // if(item.cover_i === undefined){
+        //     console.log(item.cover_i);
+        //     console.log(document.getElementById('img-div').src=`facebook_profile_image.png`);
         // }
+        // if(item.publisher!==undefined)
+        console.log(item.author_name);
         bookDisplay.appendChild(div2);
     });
 }
-
-// const imgError = image =>{
-//     image.onerror = "";
-//     image.src='facebook_profile_ima';
-//     return true;
-// }
